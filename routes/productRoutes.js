a4c2371bdc906aa401ec850d9c91eb94b7497825
PedroSzyk <szykp13@gmail.com>
2021-04-29 19:30:42 -0300

    subo todo con error en el edit

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,18 +5,20 @@ const path = require('path');
 const productController = require('../controllers/productsController');
 
 router.get('/create', productController.create);
-//router.post('/', productController.store);
-//router.get('/:id/edit', productController.edit);
-//router.put('/', productController.update);
+router.get('/:id', productController.show);
+router.get('/:id/edit', productController.edit);
+router.get('/cart', productController.cart);
+
+//router.post('/store', productController.store);
+//router.put('/:id', productController.update);
+
+//router.delete('/:id', productController.destroy);
 
 
 /* ESTO ES UN EJEMPLO HAY QUE CREAR LAS RUTAS
 
 router.get('/', productController.list);
 router.get('/search', productController.search);
-
-router.get('/:id', productController.detail);
-router.delete('/:id', productController.destroy);
 */
 
 module.exports = router;
