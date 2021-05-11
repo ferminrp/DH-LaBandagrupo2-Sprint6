8a407a1ffcd425d3bc98a47fbeda6e960497e1ab
ferminrp <frodriguezpenelas@gmail.com>
2021-05-11 13:45:55 -0300

    agrego info a la tabla del admin de productos

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,14 @@
+// ESTO SERIA EL GESTOR DEL MODELO
+const jsonDB = require('../model/jsonDatabase');
+// Maneja todos los métodos para PRODUCTO, que lo pasa como parámetro
+const productModel = jsonDB('../data/products01');
+
 module.exports = {
     admin: (req, res) => {
-        res.render('backoffice/admin')
+        res.redirect('/admin/products')
+    },
+    adminProducts: (req, res) => {
+        const products = productModel.all();
+        res.render('backoffice/admin', {products});
     }
 };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,13 @@
+// Dependencias
 const express = require('express');
 const router = express.Router();
 
-
+// Controllers
 const adminController = require('../controllers/adminController');
 
 router.get('', adminController.admin);
+
+router.get('/products', adminController.adminProducts);
 
 
 
