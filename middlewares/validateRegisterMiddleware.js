35f6b207be172427af6c785c8eccf89c5215e5ff
santiagoane <santiagoane@abacontadores.com>
2021-05-26 20:15:06 -0300

    Santi - intentando pullear

--- middlewares/validateRegisterMiddleware.js
+++ middlewares/validateRegisterMiddleware.js
@@ -1,12 +1,15 @@
 const path = require('path');
 const { body } = require('express-validator');
 
+console.log();
+
 module.exports = [
+	
 	body('name').notEmpty().withMessage('Tienes que escribir un nombre'),
 	body('email')
 		.notEmpty().withMessage('Tienes que escribir un correo electrónico').bail()
 		.isEmail().withMessage('Debes escribir un formato de correo válido'),
-	body('contraseña').notEmpty().withMessage('Tienes que escribir una contraseña'),
+	body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
 	body('avatar').custom((value, { req }) => {
 		let file = req.file;
 		let acceptedExtensions = ['.jpg', '.png', '.gif'];
